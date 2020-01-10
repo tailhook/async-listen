728d6255d7119eb4f664507461a9683cd7d14a89
Paul Colomiets <paul@colomiets.name>
2020-01-11 00:10:33 +0200

    Add metric example

---
+++ examples/metric.rs
@@ -0,0 +1,41 @@
+use std::error::Error;
+use std::time::Duration;
+
+use async_std::task;
+use async_std::net::TcpListener;
+use async_std::prelude::*;
+
+use rand::{thread_rng, Rng};
+use async_server::{ListenExt, ByteStream, backpressure};
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let (metrics, bp) = backpressure::new(10);
+    let metrics1 = metrics.clone();
+    task::spawn(async move {
+        loop {
+            task::sleep(Duration::from_secs(10)).await;
+            println!("Currently connected {}", metrics1.get_active_tokens());
+        }
+    });
+    task::block_on(async move {
+        let listener = TcpListener::bind("localhost:8080").await?;
+        eprintln!("Accepting connections on localhost:8080");
+        let mut incoming = listener.incoming()
+            .log_warnings(|e| eprintln!("Error: {}. Sleeping 0.5s...", e))
+            .handle_errors(Duration::from_millis(500))
+            .backpressure_wrapper(bp);
+        while let Some(stream) = incoming.next().await {
+            task::spawn(connection_loop(stream, metrics.clone()));
+        }
+        Ok(())
+    })
+}
+async fn connection_loop(mut stream: ByteStream, metrics: backpressure::Sender)
+{
+    let delay = Duration::from_millis(thread_rng().gen_range(100, 5000));
+    task::sleep(delay).await;
+    let text = format!("Connections {}\n", metrics.get_active_tokens());
+    stream.write_all(text.as_bytes()).await
+        .map_err(|e| eprintln!("Write error: {}", e)).ok();
+}
